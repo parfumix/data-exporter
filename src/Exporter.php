965916906e1243d79783258f2bb1f262dbea7fb2
Radu <radu.bordei89@gmail.com>
2015-08-27 17:44:27 +0300

    comments.

--- src/Exporter.php
+++ src/Exporter.php
@@ -2,22 +2,39 @@
 
 namespace DataExporter;
 
-abstract class Exporter   {
+abstract class Exporter {
 
+    /**
+     * @var array
+     */
     protected $options;
 
+    /**
+     * @var
+     */
     protected $driver;
 
     public function __construct(array $options = array()) {
         $this->options = $options;
     }
 
+    /**
+     * Set driver source .
+     *
+     * @param DriverInterface $driver
+     * @return $this
+     */
     public function setDriver(DriverInterface $driver) {
         $this->driver = $driver;
 
         return $this;
     }
 
+    /**
+     * Get driver source .
+     *
+     * @return mixed
+     */
     public function getDriver() {
         return $this->driver;
     }
