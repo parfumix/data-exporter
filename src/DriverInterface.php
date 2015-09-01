7dbde0ba8439a4139ecab3bb2fb1708c222fa094
Radu <radu.bordei89@gmail.com>
2015-09-01 12:08:58 +0300

    adding getData to contract.

--- src/DriverInterface.php
+++ src/DriverInterface.php
@@ -4,5 +4,18 @@ namespace DataExporter;
 
 interface DriverInterface {
 
+    /**
+     * Set data .
+     *
+     * @param $data
+     * @return mixed
+     */
     public function setData($data);
+
+    /**
+     * Get data .
+     *
+     * @return mixed
+     */
+    public function getData();
 }
