377092d673b4f5135cbfe377b69ce109b3e1d435
Radu <radu.bordei89@gmail.com>
2015-09-01 12:09:16 +0300

    improve setData to Eloquent driver.

--- src/Drivers/Eloquent.php
+++ src/Drivers/Eloquent.php
@@ -4,10 +4,40 @@ namespace DataExporter\Drivers;
 
 use DataExporter\Driver;
 use DataExporter\DriverInterface;
+use DataExporter\Eloquent\Exportable;
 
 class Eloquent extends Driver implements DriverInterface {
 
-    public function setData($data) {
-        // TODO: Implement setData() method.
+    /**
+     * Set data to eloquent driver .
+     *
+     * @param $model
+     * @return $this
+     */
+    public function setData($model) {
+        $data = array();
+
+        if( $model instanceof Exportable )
+            $columns = $model->getExportColumns();
+        else
+            $columns = $model->getFillable();
+
+        if( isset($model->id) ) {
+            foreach ($columns as $column => $options) {
+                $data[$column] = $model->{$column};
+            }
+        } else {
+            $rows = $model->all();
+
+            foreach($rows as $row) {
+                foreach ($columns as $column => $options) {
+                    $data[$column] = $row->{$column};
+                }
+            }
+        }
+
+        $this->data = $data;
+
+        return $this;
     }
 }
