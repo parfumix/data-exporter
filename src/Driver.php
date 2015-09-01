1e80c1cc4eafc40222a64af43e9e8b8d95d052c8
Radu <radu.bordei89@gmail.com>
2015-09-01 12:08:48 +0300

    adding setData to abstract driver.

--- src/Driver.php
+++ src/Driver.php
@@ -4,8 +4,22 @@ namespace DataExporter;
 
 abstract class Driver {
 
+    /**
+     * @var
+     */
+    protected $data;
+
     public function __construct($data) {
         $this->setData($data);
+    }
+
+    /**
+     * Get data .
+     *
+     * @return mixed
+     */
+    public function getData() {
+        return $this->data;
     }
 
 }
