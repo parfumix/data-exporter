729567723822eec02bf6c4ae07c10b62845c151d
Radu <radu.bordei89@gmail.com>
2015-09-01 14:30:51 +0300

    improve exportableTrait.

--- src/Eloquent/ExportableTrait.php
+++ src/Eloquent/ExportableTrait.php
@@ -2,7 +2,20 @@
 
 namespace DataExporter\Eloquent;
 
+use Flysap\Scaffold\ScaffoldAble;
+
 trait ExportableTrait {
 
-    
+    /**
+     * Get columns for exporting .
+     *
+     * @return mixed
+     */
+    public function getExportColumns() {
+        if( $this instanceof ScaffoldAble )
+            return $this->scaffoldListing();
+
+        return $this->getFillable();
+    }
+
 }
