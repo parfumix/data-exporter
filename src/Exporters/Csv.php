3a33551a80e63fc34e1bd80c9ab97ef80992f18b
Radu <radu.bordei89@gmail.com>
2015-09-02 10:24:05 +0300

    improve exporters.

--- src/Exporters/Csv.php
+++ src/Exporters/Csv.php
@@ -15,14 +15,35 @@ class Csv extends Exporter implements ExporterInterface {
         );
     }
 
-    public function export($filename = null) {
+    /**
+     * Export data ..
+     *
+     * @param $filename
+     * @param callable $callback
+     * @return mixed
+     */
+    public function export($filename = null, \Closure $callback = null) {
         $fullPath = $this->fullPath($filename);
 
         $fileInfo = pathinfo($fullPath);
 
-        $this->getWriter()->create(
-            $fileInfo['filename']
-        )->save('csv', $fileInfo['dirname']);
+        $this->getWriter()->create($fileInfo['filename'], function($excel) use($callback, $filename) {
+            $data = $this->getDriver()->getData();
+
+            if(! is_null($callback)) {
+                $callback($excel, $data);
+            } else {
+                $excel->sheet($filename, function($sheet) use($data) {
+
+                    if( $template = $this->getTemplate() ) {
+                        if( $font = $template->getFontStyle() )
+                            $sheet->setFont($font);
+                    }
+
+                    $sheet->fromArray($data);
+                });
+            }
+        })->save('xls', $fileInfo['dirname']);
 
         return $fullPath;
     }
--- src/Exporters/Xls.php
+++ src/Exporters/Xls.php
@@ -20,16 +20,31 @@ class Xls extends Exporter implements ExporterInterface {
      * Export data ..
      *
      * @param $filename
+     * @param callable $callback
      * @return mixed
      */
-    public function export($filename = null) {
+    public function export($filename = null, \Closure $callback = null) {
         $fullPath = $this->fullPath($filename);
 
         $fileInfo = pathinfo($fullPath);
 
-        $this->getWriter()->create(
-            $fileInfo['filename']
-        )->save('xls', $fileInfo['dirname']);
+        $this->getWriter()->create($fileInfo['filename'], function($excel) use($callback, $filename) {
+            $data = $this->getDriver()->getData();
+
+            if(! is_null($callback)) {
+                $callback($excel, $data);
+            } else {
+                $excel->sheet($filename, function($sheet) use($data) {
+
+                    if( $template = $this->getTemplate() ) {
+                        if( $font = $template->getFontStyle() )
+                            $sheet->setFont($font);
+                    }
+
+                    $sheet->fromArray($data);
+                });
+            }
+        })->save('xls', $fileInfo['dirname']);
 
         return $fullPath;
     }
