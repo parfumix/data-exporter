8ea4a9cd58bc82569868f629c3ca2336d33f30b8
Radu <radu.bordei89@gmail.com>
2015-09-02 10:24:15 +0300

    adding callback closure.

--- src/ExporterInterface.php
+++ src/ExporterInterface.php
@@ -8,15 +8,17 @@ interface ExporterInterface {
      * Save to disk .
      *
      * @param $path
+     * @param callable $callback
      * @return mixed
      */
-    public function export($path = null);
+    public function export($path = null, \Closure $callback = null);
 
     /**
      * Download export attachment .
      *
      * @param null $path
+     * @param callable $callback
      * @return mixed
      */
-    public function download($path = null);
+    public function download($path = null, \Closure $callback = null);
 }
